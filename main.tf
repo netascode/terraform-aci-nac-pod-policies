c884d1b4535942a267ac9fa955333ee3f3ec480f
danischm <danischm@cisco.com>
2023-01-14 13:55:24 +0100

    Refactor lookups

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 locals {
-  modules      = lookup(var.model, "modules", {})
-  pod_policies = lookup(lookup(var.model, "apic", {}), "pod_policies", {})
+  modules      = try(var.model.modules, {})
+  pod_policies = try(var.model.apic.pod_policies, {})
 }
 
 module "aci_pod_setup" {
   source  = "netascode/pod-setup/aci"
   version = "0.1.0"
 
-  for_each = { for pod in lookup(local.pod_policies, "pods", {}) : pod.id => pod if lookup(local.modules, "aci_pod_setup", true) }
+  for_each = { for pod in try(local.pod_policies.pods, []) : pod.id => pod if try(local.modules.aci_pod_setup, true) }
   pod_id   = each.value.id
-  tep_pool = lookup(each.value, "tep_pool", null)
+  tep_pool = try(each.value.tep_pool, null)
 }
